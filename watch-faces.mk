597a16bae1272c26cb3501b76dd98b675270b0b6
Chris Gillings <git@ned-ludd.com>
2026-09-06 01:26:35 +1000

    Remove face that belongs in another branch

--- watch-faces.mk
+++ watch-faces.mk
@@ -11,7 +11,6 @@ SRCS += \
   ./watch-faces/complication/stopwatch_face.c \
   ./watch-faces/complication/fast_stopwatch_face.c \
   ./watch-faces/complication/sunrise_sunset_face.c \
-  ./watch-faces/complication/twilight_face.c \
   ./watch-faces/complication/moon_phase_face.c \
   ./watch-faces/complication/days_since_face.c \
   ./watch-faces/complication/breathing_face.c \
